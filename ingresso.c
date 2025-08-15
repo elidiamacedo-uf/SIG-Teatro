8e1494b92591b13df6fc17c79da69a4a8ca41e27
Marcos <marcosviniciuscosmo13@gmail.com>
2025-08-15 16:53:31 -0300

    Adicionado Animação ao módulo

--- ingresso.c
+++ ingresso.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int tempo = 150000;
 int main(){
     int executar;
     printf("#####################################################################################\n");
+    usleep(tempo);
     printf("   +++                                INGRESSO                                 +++\n");
+    usleep(tempo);
     printf("#####################################################################################\n");
+    usleep(tempo);
     printf("###                             1 - VENDER INGRESSO                               ###\n");
+    usleep(tempo);
     printf("###                                                                               ###\n");
+    usleep(tempo);
     printf("###                             2 - EXCLUIR INGRESSO                              ###\n");
+    usleep(tempo);
     printf("###                                                                               ###\n");
+    usleep(tempo);
     printf("###                             3 - ALTERAR INGRESSO                              ###\n");
+    usleep(tempo);
     printf("###                                                                               ###\n");
+    usleep(tempo);
     printf("###                             4 - PESUISAR SHOW                                 ###\n");
+    usleep(tempo);
     printf("###                                                                               ###\n");
+    usleep(tempo);
     printf("##                              0 - SAIR : ");
     scanf("%d",&executar);
     return 0;
