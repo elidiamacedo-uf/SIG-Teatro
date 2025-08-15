d09562ca161515a9771dd961503d83dd5d39e764
Marcos <marcosviniciuscosmo13@gmail.com>
2025-08-15 14:28:09 -0300

    Continuando o Módulo Ingresso

--- ingresso.c
+++ ingresso.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 
 int main(){
+    int executar;
     printf("#####################################################################################\n");
     printf("   +++                                INGRESSO                                 +++\n");
     printf("#####################################################################################\n");
     printf("###                             1 - VENDER INGRESSO                               ###\n");
-    printf("\n");
+    printf("###                                                                               ###\n");
     printf("###                             2 - EXCLUIR INGRESSO                              ###\n");
+    printf("###                                                                               ###\n");
+    printf("###                             3 - ALTERAR INGRESSO                              ###\n");
+    printf("###                                                                               ###\n");
+    printf("###                             4 - PESUISAR SHOW                                 ###\n");
+    printf("###                                                                               ###\n");
+    printf("##                              0 - SAIR : ");
+    scanf("%d",&executar);
     return 0;
 }
