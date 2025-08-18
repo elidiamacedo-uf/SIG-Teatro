3c91632c6d66379f59feedf38f8ae48249fccb0d
elidiamacedo-uf <elidia.macedo.707@ufrn.edu.br>
2025-08-18 15:16:38 -0300

    switch case

---
+++ tecnicos.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void tecnicos(){
+    int escolha;
+
+        printf("###                             1 - CADASTRAR TÉCNICO                              ###\n");
+        printf("###                             2 - PROCURAR TÉCNICO                             ###\n");
+        printf("###                             3 - ATUALIZAR TÉCNICO                           ###\n");
+        printf("###                             4 - DELETAR TÉCNICO                           ###\n");
+        printf("###                             0 - VOLTAR AO MENU ANTERIOR...                      ###\n");
+        scanf("%d", &escolha);
+
+        switch (escolha)
+        {
+        case 1:
+            printf("1 - Cadastrar técnico\n");
+            break;
+        case 2:
+            printf("2 - Procurar técnico\n");
+            break;
+        case 3:
+            printf("3 - Atualizar técnico\n");
+            break;
+        case 4:
+            printf("4 - Deletar técnico\n");
+            break;
+        case 0:
+            printf("0 - Voltar ao anterior...\n");
+            break;
+        default:
+            printf("Opção inválida ");
+            break;
+        }
+    }
+
+int main(){
+    tecnicos();
+    return 0;
+}
