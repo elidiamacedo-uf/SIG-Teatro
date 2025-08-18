1c96a755986a0b966cfa7e78a040d1bc216fe301
elidiamacedo-uf <elidia.macedo.707@ufrn.edu.br>
2025-08-18 14:12:47 -0300

    interface 3 - sessoes

---
+++ sessoes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void sessoes(){
+        printf("###                             1 - CRIAR SESSÃO                              ###\n");
+        printf("###                             2 - PROCURAR SESSÃO                              ###\n");
+        printf("###                             3 - ATUALIZAR SESSÃO                              ###\n");
+        printf("###                             4 - DELETAR SESSÃO                              ###\n");
+}
+
+
+
+
+int main(){
+    sessoes();
+    return 0;
+}
