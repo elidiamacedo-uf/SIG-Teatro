9b663f1b1c494114ebca910eeb0761601464ba89
Marcos <marcosviniciuscosmo13@gmail.com>
2025-08-18 23:14:13 -0300

    Adicionado o Módulo Shows

--- principal.c
+++ principal.c
@@ -25,6 +25,12 @@ void excluir_Ingresso();
 void atualizar_Ingresso();
 void pesquisar_Ingresso();
 
+void shows();
+void cadastrar_Show();
+void excluir_Show();
+void atualizar_Show();
+void pesquisar_Show();
+
 void sessoes();
 void criar_Sessao();
 void procurar_Sessao();
@@ -79,6 +85,7 @@ int main() {
                 ingresso();
                 break;
             case '2':
+                shows();
                 break;
             case '3':
                 sessoes();
@@ -98,6 +105,7 @@ int main() {
     } 
 return 0; 
 }
+
 
 
 void ingresso(){  
@@ -142,6 +150,62 @@ void ingresso(){
 
             case '4':
                 pesquisar_Ingresso();
+            case '0':
+                main();
+                break;
+            default:
+                printf("\n \n");
+                printf("!VALOR INVÁLIDO, POR FAVOR INSERIR OS UM DOS VALORES ACIMA!\n");
+                system("pause");
+    }
+  }
+}
+
+void shows(){  
+    int tempo = 100;
+    bool parada_S = true;
+    while (parada_S){  
+        char executar_S;
+        system("clear||cls");
+        printf("#####################################################################################\n");
+        func_Ani(tempo);
+        printf("   +++                                  SHOWS                                 +++\n");
+        func_Ani(tempo);
+        printf("#####################################################################################\n");
+        func_Ani(tempo);
+        printf("###                             1 - CADASTRAR SHOW                                ###\n");
+        func_Ani(tempo);
+        printf("###                                                                               ###\n");
+        func_Ani(tempo);
+        printf("###                             2 - EXCLUIR SHOW                                  ###\n");
+        func_Ani(tempo);
+        printf("###                                                                               ###\n");
+        func_Ani(tempo);
+        printf("###                             3 - ATUALIZAR SHOW                                ###\n");
+        func_Ani(tempo);
+        printf("###                                                                               ###\n");
+        func_Ani(tempo);
+        printf("###                             4 - PESQUISAR SHOW                                ###\n");
+        func_Ani(tempo);
+        printf("###                                                                               ###\n");
+        func_Ani(tempo);
+        printf("##                              0 - SAIR : ");
+        scanf(" %c",&executar_S);
+
+        switch (executar_S)
+        {
+            case '1':
+                cadastrar_Show();
+                break;
+            case '2':
+                excluir_Show();
+                break;
+            case '3':
+                atualizar_Show();
+                break;
+            case '4':
+                pesquisar_Show();
+                break;
             case '0':
                 main();
                 break;
@@ -222,7 +286,7 @@ void sessoes(){
 
 
 
-
+//############### FUNÇÕES MÓDULO INGRESSO ##################################################
 void vender_Ingresso(){
     system("clear||cls");
     int i;
@@ -269,7 +333,6 @@ void vender_Ingresso(){
 
 }
 
-
 void excluir_Ingresso(){
     system("clear||cls");
     int i;
@@ -315,7 +378,6 @@ void excluir_Ingresso(){
     ingresso();
 }
 
-
 void atualizar_Ingresso(){
     system("clear||cls");
     int i;
@@ -360,7 +422,6 @@ void atualizar_Ingresso(){
 
     ingresso();
 }
-
 
 void pesquisar_Ingresso(){
     system("clear||cls");
@@ -408,10 +469,190 @@ void pesquisar_Ingresso(){
 }
 
 
+//############### FUNÇÕES MÓDULO SHOWS #####################################################
+void cadastrar_Show(){
+    system("clear||cls");
+    int i;
+     //Primeira Linha
+    for(i = 0; i<70; i++){  
+        printf("#");
+        func_Ani(1);
+    }
+    printf("\n");
+
+    //Segunda Linha
+    for(i = 0;i<27; i++){
+        if (i > 3 && i < 7){ 
+            printf("+");
+            func_Ani(15);
+        } 
+        else{
+            printf(" ");
+            func_Ani(15);
+        }
+    }
+    printf("CADASTRAR SHOW");
+    
+    for(i = 0;i<31;i++){
+        if (i > 19 && i < 24){
+            printf("+");
+            func_Ani(15);
+        }
+        else{
+            printf(" ");
+            func_Ani(1);
+        }
+    }
+    printf("\n");
+
+    // Terceira Linha
+    for(i = 0; i<70; i++){  
+        printf("#");
+        func_Ani(1);
+    }
+    printf("\n");
+
+    shows();
+
+}
+
+void excluir_Show(){
+     system("clear||cls");
+    int i;
+     //Primeira Linha
+    for(i = 0; i<70; i++){  
+        printf("#");
+        func_Ani(1);
+    }
+    printf("\n");
+
+    //Segunda Linha
+    for(i = 0;i<27; i++){
+        if (i > 3 && i < 7){ 
+            printf("+");
+            func_Ani(15);
+        } 
+        else{
+            printf(" ");
+            func_Ani(15);
+        }
+    }
+    printf("EXCLUIR SHOW");
+    
+    for(i = 0;i<31;i++){
+        if (i > 19 && i < 24){
+            printf("+");
+            func_Ani(15);
+        }
+        else{
+            printf(" ");
+            func_Ani(1);
+        }
+    }
+    printf("\n");
+
+    // Terceira Linha
+    for(i = 0; i<70; i++){  
+        printf("#");
+        func_Ani(1);
+    }
+    printf("\n");
+
+    shows();
+}
+
+void atualizar_Show(){
+     system("clear||cls");
+    int i;
+     //Primeira Linha
+    for(i = 0; i<70; i++){  
+        printf("#");
+        func_Ani(1);
+    }
+    printf("\n");
+
+    //Segunda Linha
+    for(i = 0;i<27; i++){
+        if (i > 3 && i < 7){ 
+            printf("+");
+            func_Ani(15);
+        } 
+        else{
+            printf(" ");
+            func_Ani(15);
+        }
+    }
+    printf("ATUALIZAR SHOW");
+    
+    for(i = 0;i<31;i++){
+        if (i > 19 && i < 24){
+            printf("+");
+            func_Ani(15);
+        }
+        else{
+            printf(" ");
+            func_Ani(1);
+        }
+    }
+    printf("\n");
+
+    // Terceira Linha
+    for(i = 0; i<70; i++){  
+        printf("#");
+        func_Ani(1);
+    }
+    printf("\n");
+
+    shows();
+}
+
+void pesquisar_Show(){
+     system("clear||cls");
+    int i;
+     //Primeira Linha
+    for(i = 0; i<70; i++){  
+        printf("#");
+        func_Ani(1);
+    }
+    printf("\n");
+
+    //Segunda Linha
+    for(i = 0;i<27; i++){
+        if (i > 3 && i < 7){ 
+            printf("+");
+            func_Ani(15);
+        } 
+        else{
+            printf(" ");
+            func_Ani(15);
+        }
+    }
+    printf("PESQUISAR SHOW");
+    
+    for(i = 0;i<31;i++){
+        if (i > 19 && i < 24){
+            printf("+");
+            func_Ani(15);
+        }
+        else{
+            printf(" ");
+            func_Ani(1);
+        }
+    }
+    printf("\n");
+
+    // Terceira Linha
+    for(i = 0; i<70; i++){  
+        printf("#");
+        func_Ani(1);
+    }
+    printf("\n");
+
+    shows();
+}
 
 
-
-
+//############### FUNCÕES MÓDULO TÉCNICO ###################################################
 void cadastro_Tecnico(){
     system("clear || cls");
     printf("CPF: ");
@@ -439,7 +680,7 @@ void excluir_Tecnico(){
 }
 
 
-
+//############### FUNÇÕES MÓDULO TÉCNICO ###################################################
 void criar_Sessao(){
     system("clear || cls");
     printf("ID SESSÃO: ");
@@ -461,6 +702,7 @@ void excluir_Sessao(){
 }
 
 
+//################# FUNÇÕES DE ANIMAÇÃO ##################################################
 void ani_Encerrar(){
     printf("\n \n");
     
