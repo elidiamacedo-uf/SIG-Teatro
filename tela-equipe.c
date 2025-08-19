f941f432f4183f086df4c442fe04260a7c46cdf7
elidiamacedo-uf <elidia.macedo.707@ufrn.edu.br>
2025-08-18 21:23:01 -0300

    tela equipe do projeto

---
+++ tela-equipe.c
@@ -0,0 +1,14 @@
+void equipe(){
+    printf("             Universidade Federal do Rio Grande do Norte                 \n");
+    printf("                 Disciplina DCT1106 -- Programação                       \n");
+    printf("                   Professor: Flavius Gorgônio                           \n");
+    printf("#########################################################################\n");
+    printf("-------------------- Sistema de Gestão de Teatro -------------------------\n");
+    printf("#########################################################################\n");
+    printf("\n");
+    printf("             Projeto desenvolvido por:                                   \n");
+    printf("             Aluno 1: Emilly Elídia Andrade Macedo dos Santos            \n");
+    printf("             Aluno 2: Marcos Vinícius Dantas da Silva                    \n");
+    printf("\n");
+    printf("#########################################################################\n");
+}
